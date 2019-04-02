90ab32d78a5db2f20b60b9788ca0d4d9c8811e97
Andry Mironov <andry.mironov94@gmail.com>
2019-04-02 19:25:42 +0300

    Week 4. Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import {
 	Card,
 	CardImg,
@@ -16,8 +17,7 @@ import {
 	Col
 } from 'reactstrap'
 import { Control, LocalForm, Errors } from 'react-redux-form'
-
-import { Link } from 'react-router-dom'
+import { Loading } from './LoadingComponent'
 
 const required = val => val && val.length
 const maxLength = len => val => !val || val.length <= len
@@ -167,8 +167,23 @@ class CommentForm extends React.Component {
 }
 
 const DishDetail = props => {
-	const dish = props.dish
-	if (dish)
+	if (props.isLoading) {
+		return (
+			<div className="container">
+				<div className="row">
+					<Loading />
+				</div>
+			</div>
+		)
+	} else if (props.errMess) {
+		return (
+			<div className="container">
+				<div className="row">
+					<h4>{props.errMess}</h4>
+				</div>
+			</div>
+		)
+	} else if (props.dish != null)
 		return (
 			<div className="container">
 				<div className="row">
